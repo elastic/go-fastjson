06d3553f0d27ba32223877fc1c6d8fda6a52fdd0
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-12-15 17:26:17 +0000

    go_import_path for 1.8

--- .ci/test.sh
+++ .ci/test.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+## beginning go_import_path
+mkdir -p "${GOPATH}/src/go.elastic.co/fastjson"
+cp -Rf . "${GOPATH}/src/go.elastic.co/fastjson"
+cd $GOPATH/src/go.elastic.co/fastjson
+go get
+cd -
+## end go_import_path
+
 # Run the tests
 set +e
 export OUT_FILE="build/test-report.out"
